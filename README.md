c6d8daf454c405d68652e0fc584c38c2875cb1e5
Drew Hart <drew.ellison.hart@gmail.com>
2021-04-28 20:13:15 -0700

    moving my nascent WoS work to GitHub, in a new repo for our agrofor lit-review work

---
+++ README.md
@@ -0,0 +1,13 @@
+## Code and small static data files used for our 'state of the knowledge' lit review on agroforestry as an NCS.
+
+**Contains**:
+
+ - static files containing WoS search terms for:
+   - agroforestry systems
+   - natural climate solutions (and related language)
+   - carbon sequestration estimates
+ - a Python script to glue those terms together in order to search for:
+   - literature studying agroforestry as an NCS
+   - literature providing carbon sequestration estimates in agroforestry
+ - an Excel file of agroforestry search terms that Sam pulled from \_\_\_WHERE?\_\_\_
+ - a Python script that chopped some info out of that Excel file and processed it a bit
---
+++ create_agrofor_search_terms.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+topic = sys.argv[1]
+
+assert topic in ['ncs', 'carbon'], 'VALID TOPICS: "ncs", "carbon"'
+
+
+def read_search_terms_file(topic):
+    """
+    Reads in a search-terms file.
+
+    Params
+    ------
+    topic : str
+        can be 'agrofor', 'ncs', or 'carbon'
+
+    Returns
+    -------
+    txt : str
+        the file's search-terms contents, as a block of text
+
+    """
+    with open('search_terms_%s.txt' % topic, 'r') as f:
+        txt = f.read()
+    return txt
+
+
+if __name__ == '__main__':
+    # read in the search-term blocks
+    agrofor_terms = read_search_terms_file('agrofor')
+    topic_terms = read_search_terms_file(topic)
+
+    # peg them together
+    search_terms = 'TS=(\n\n%s\nAND\n\n%s\n)' % (agrofor_terms, topic_terms)
+
+    os.system("echo '%s' | xclip -sel clip" % search_terms)
