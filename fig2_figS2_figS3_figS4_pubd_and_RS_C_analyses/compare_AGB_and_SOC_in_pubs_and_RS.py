Fixed and finalized Fig. 2b follow-on spatial precision analysis. Finalized Fig. 3. Tweaked other files, and continued cleaning up. Ran final versions of all analyses.

fig2_figS2_figS3_figS4_pubd_and_RS_C_analyses/compare_AGB_and_SOC_in_pubs_and_RS.py
@@ -571,69 +571,6 @@ for var in var_dict.keys():
 # make figure s3
 ################
 
-# assess variance in divergence from 1:1 line as fn of geo coord precision
-def calc_coord_precision(coord):
-    ct = 0
-    # return 0 if no decimals
-    if '.' not in str(coord):
-        return ct
-    str_dec = str(coord).split('.')[1]
-    for i, dig in enumerate(str_dec):
-        ct += 1
-        if i+1 == len(str_dec):
-            return ct
-        elif i+2 == len(str_dec):
-            if str_dec[i+1] == dig:
-                return ct
-            else:
-                return ct+1
-        else:
-            if str_dec[i+2] == str_dec[i+1] == dig:
-                return ct
-
-def calc_coord_precision_column(df):
-    lat_prec = [calc_coord_precision(lat) for lat in df.lat]
-    lon_prec = [calc_coord_precision(lon) for lon in df.lon]
-    prec_col = np.max(np.vstack((np.array(lat_prec), np.array(lon_prec))), axis=0)
-    # reclass the numeric coord_prec
-    class_bin_lefts= [1, 2, 3]
-    class_bin_rights = [2, 3, 10000]
-    class_bins = ['low', 'mod', 'high']
-    prec_bin_col = []
-    for val in prec_col:
-        for bin, l, r in zip(class_bins, class_bin_lefts, class_bin_rights):
-            if l<= val < r:
-                prec_bin_col.append(bin)
-    assert len(prec_bin_col) == len(prec_col)
-    return prec_col, prec_bin_col
-
-prec_col, prec_bin_col = calc_coord_precision_column(agb_comp)
-agb_comp['coord_prec'] = prec_col
-agb_comp['coord_prec_bin'] = prec_bin_col
-
-# print variances for low, mod, and high-precision coordinates,
-# and print results of Bartlett's test for equal variances
-# (for all normally distributed samples, since histograms look close enough)
-for _ in range(3):
-    print('-'*80)
-print('\n\n')
-print(('\n\nStandard deviations of differences between WHRC '
-       'remotely sensed estimates and\npublished AGC '
-       'for samples with different coordinate precisions:\n'))
-bins = ['low', 'mod', 'high']
-for prec_bin in bins:
-    std = agb_comp[agb_comp.coord_prec_bin==prec_bin]['stock_diff_whrc'].std()
-    print('\n\tprecision: %s\n\n\tstd: %0.4f\n\n' % (prec_bin, std))
-
-print(("\n\nResults of Levene's test of equal variances (for "
-       "non-normally-distributed samples):\n\n"))
-samples = [agb_comp[agb_comp['coord_prec_bin']==b][
-                                    'stock_diff_whrc'].values for b in bins]
-samples = [samp[np.invert(np.isnan(samp))] for samp in samples]
-levene = stats.levene(*samples)
-print('\n\tstat: %0.4f\n' % levene.statistic)
-print('\n\tp-value: %0.4f\n' % levene.pvalue)
-
 # assess correlation between divergence from 1:1 line and divergence of 2000
 # RS year from published measurement year
 for _ in range(3):
@@ -687,6 +624,115 @@ fig_yr_diff.subplots_adjust(left=0.2, right=0.97, bottom=0.15, top=0.93,
 fig_yr_diff.savefig('FIGS3_regression_WHRC_Cardinael_stock_diff_vs_meas_yr_diff.png',
                dpi=dpi)
 fig_yr_diff.show()
+
+# display % of measurements in 0, (0,5], (5,10], and (10, inf) year-diff bins
+pct_0 = len(agb_comp[(agb_comp['meas_yr_diff']==0)])/len(agb_comp)
+pct_0to5 = len(agb_comp[(np.abs(agb_comp['meas_yr_diff'])>0) &
+                        (np.abs(agb_comp['meas_yr_diff'])<=5)])/len(agb_comp)
+pct_5to10 = len(agb_comp[(np.abs(agb_comp['meas_yr_diff'])>5) &
+                         (np.abs(agb_comp['meas_yr_diff'])<=10)])/len(agb_comp)
+pct_gt10 = len(agb_comp[(agb_comp['meas_yr_diff']>10)])/len(agb_comp)
+assert np.allclose(np.sum((pct_0, pct_0to5, pct_5to10, pct_gt10)), 1)
+print('\n\n%0.2f%% measurements with no year difference' % (100*pct_0))
+print('\n\n%0.2f%% measurements with 0 < year difference <= 5' % (100*pct_0to5))
+print('\n\n%0.2f%% measurements with 5 < year difference <= 10' % (100*pct_5to10))
+print('\n\n%0.2f%% measurements with year difference > 10' % (100*pct_gt10))
+
+
+################
+# make figure s4
+################
+
+# assess variance in divergence from 1:1 line as fn of geo coord precision
+def estimate_coord_precision(coord):
+    ct = 0
+    # return 0 if no decimals
+    if '.' not in str(coord):
+        return ct
+    str_dec = str(coord).split('.')[1]
+    for i, dig in enumerate(str_dec):
+        ct += 1
+        if i+1 == len(str_dec):
+            return ct
+        elif i+2 == len(str_dec):
+            if str_dec[i+1] == dig:
+                return ct
+            else:
+                return ct+1
+        else:
+            if str_dec[i+2] == str_dec[i+1] == dig:
+                return ct
+
+def calc_coord_precision_column(df):
+    lat_prec = [estimate_coord_precision(lat) for lat in df.lat]
+    lon_prec = [estimate_coord_precision(lon) for lon in df.lon]
+    prec_col = (np.array(lat_prec) + np.array(lon_prec))/2
+    # reclass the numeric coord_prec
+    class_bin_lefts= [1, 2, 3]
+    class_bin_rights = [2, 3, 10000]
+    class_bins = ['low', 'mid', 'high']
+    prec_bin_col = []
+    for val in prec_col:
+        for bin, l, r in zip(class_bins, class_bin_lefts, class_bin_rights):
+            if l<= val < r:
+                prec_bin_col.append(bin)
+    assert len(prec_bin_col) == len(prec_col)
+    return prec_col, prec_bin_col
+
+prec_col, prec_bin_col = calc_coord_precision_column(agb_comp)
+agb_comp['coord_prec'] = prec_col
+agb_comp['coord_prec_bin'] = prec_bin_col
+
+# drop the few columns with precision > 6 (given that this is ~1.1 m precision,
+# standard circular accuracy for many GPS units, whereas a couple rows have
+# precision of 12, tantamount to ~micron precision! this was in original
+# dataset, so unclear where this originated upstream...)
+agb_comp = agb_comp[agb_comp['coord_prec']<=6]
+
+# add absolute stock-diff column
+agb_comp['stock_diff_whrc_abs'] = np.abs(agb_comp['stock_diff_whrc'])
+
+# assess correlation between coordinate precision and disagreement between
+# published and RS estimates
+for _ in range(3):
+    print('-'*80)
+print('\n\n')
+fig_prec, ax_prec = plt.subplots(1, 1, figsize=(6.5,6.5))
+for prac in pracs:
+    sub_agb_comp = agb_comp[agb_comp['practice'] == prac]
+    sns.scatterplot(x='coord_prec',
+                    y='stock_diff_whrc_abs',
+                    hue='practice',
+                    palette=[prac_colors[prac]],
+                    edgecolor='black',
+                    s=30,
+                    alpha=0.5,
+                    data=sub_agb_comp,
+                    legend=True,
+                    ax=ax_prec)
+sns.regplot(x='coord_prec', y='stock_diff_whrc_abs', data=agb_comp, scatter=False)
+ax_prec.set_xlabel(('approximate precision of published geographic '
+                       '\ncoordinates (decimal degrees)'), fontdict={'fontsize': 12})
+ax_prec.set_ylabel(('absolute stock estimate difference ($Mg\ C\ ha^{-1}$)\n'
+                       '|Cardinael data stock estmate - '
+                       'WHRC stock estimate|'), fontdict={'fontsize':12})
+ax_prec.tick_params(labelsize=8)
+agb_comp_reg = agb_comp.loc[:, ['stock_diff_whrc_abs', 'coord_prec']].dropna()
+print(('\n\nResults of regression of stock-estimate difference on coordinate-'
+       'precision:\n\n'))
+mod = OLS(agb_comp_reg['stock_diff_whrc_abs'],
+          np.vstack((np.ones(len(agb_comp_reg)),
+                     agb_comp_reg['coord_prec'])).T).fit()
+print('\n\tintercept: %0.4f $Mg\ C\ ha^{-1} (p=%0.2e)' % (mod.params['const'],
+                                          mod.pvalues['const']))
+print('\n\tslope: %0.4f $Mg\ C\ ha^{-1} yr^{-1} (p=%0.2e)' % (mod.params['x1'],
+                                      mod.pvalues['x1']))
+print('\n\tR-squared: %0.4f' % mod.rsquared)
+fig_prec.subplots_adjust(left=0.2, right=0.97, bottom=0.15, top=0.93,
+                            wspace=0, hspace=0)
+fig_prec.savefig(('FIGS4_regression_WHRC_Cardinael_stock_diff_vs_'
+                  'coord_precision.png'), dpi=dpi)
+fig_prec.show()
 
 
 

fig3_maps_fig5_curr_vs_poten/AF_locations_from_papers/prep_AF_location_data.py
@@ -31,7 +31,7 @@ df.columns = [col.replace('.', '_') for col in df.columns]
 
 # function for handling strings with degree and minute signs in them
 def handle_deg_min_str(val):
-    dir = re.split(' ', val.strip())[-1]
+    dir = re.split(' ', val.strip())[-1].strip()
     assert dir in ['E', 'W', 'N', 'S']
     split_vals = [substr.strip() for substr in re.split('to', val)]
     assert len(split_vals) == 2
@@ -42,7 +42,7 @@ def handle_deg_min_str(val):
         deg = float(deg)
         min = float(min)
         dec_val = deg + (min /60)
-        if dir in ['W', 'S']:
+        if dir.strip() in ['W', 'S']:
             dec_val *= -1
         dec_vals.append(dec_val)
     assert len(dec_vals) == 2
@@ -67,7 +67,8 @@ def handle_NSEW_str(val):
 # decimal-degree lat, lon values, if decimal-degree coordinates are initially
 # missing, else just return existing decimal-degree lat/lon values
 def calc_dec_lat_lon(row):
-    if pd.notnull(row['lat']) and pd.notnull(row['lon']):
+    if (pd.notnull(row['lat']) and pd.notnull(row['lon']) and
+        len(str(row['lat']).strip()) > 0 and len(str(row['lon']).strip()) > 0):
         dec_vals = [row['lat'], row['lon']]
         for i, val in enumerate(dec_vals):
             if (isinstance(val ,str) and
@@ -106,7 +107,7 @@ def calc_dec_lat_lon(row):
                 # decimal degrees
                 deg, min, sec, dir = row[cols].values
                 dec = deg + (min + (sec/60))/60
-                if dir in ['W', 'S']:
+                if dir.strip() in ['W', 'S']:
                     dec *= -1
                 dec_vals.append(dec)
         except Exception:
@@ -149,9 +150,12 @@ df['lon_trunc'] = np.round(df['lon'] ,n_coords_dec_for_dedup)
 # remove duplicate coords (out to specifed number of decimal places)
 df = df.drop_duplicates(subset = ['lon_trunc', 'lat_trunc'])
 
-# cull problem site.id values
+# cull problem site.id values (errors trace back to original publications, so
+# cannot by corrected by us)
 df =df[df.site_id != '17169']
 df =df[df.site_id != 17169]
+df =df[df.site_id != '19419']
+df =df[df.site_id != 19419]
 
 # scatter points, if requested
 if plot_it:

fig3_maps_fig5_curr_vs_poten/map_and_analyze_current_and_potential_AF.py
@@ -101,7 +101,7 @@ af_locs = gpd.read_file(('AF_locations_from_papers/'
 #  https://raw.githubusercontent.com/milliechapman/treesincroplands/
 #  master/data/IUCN_ndc_agroforestry.csv)
 # then fold into the Rosenstock data to supplement it
-iucn = pd.read_csv(('./rosenstock_et_al_2019_data/'
+iucn = pd.read_csv(('./rosenstock_data/'
                     'supplemental_IUCN_NDC_data_from_Chapman_supps.csv'))
 iucn = iucn.iloc[:, :3]
 supplemented_NDCmnt = []
@@ -305,7 +305,6 @@ lesiv_rast = rxr.open_rasterio(os.path.join(rast_datadir,
                                chunks=(5,5),
                               )[0]
 lesiv_rast = lesiv_rast.rio.clip_box(map_minx, map_miny, map_maxx, map_maxy)
-lesiv_rast = (lesiv_rast==53).astype(np.int8)
 lesiv_rast = lesiv_rast.where(lesiv_rast==1, np.nan)
 # figure out if each location is covered by Chapman data or not
 in_lesiv = []
@@ -463,7 +462,7 @@ if make_map:
     # load high-res countries, just for plots
     countries_hi_res = gpd.read_file('./country_bounds/NewWorldFile_2020.shp')
 
-    fig_0 = plt.figure(figsize=(12,15))
+    fig_0 = plt.figure(figsize=(12,14))
     gs = fig_0.add_gridspec(2, 1, height_ratios=[1.1, 1])
 
     def format_map_axes(ax, bcax=None, max_tickval=None, add_latlon_lines=False):
@@ -553,9 +552,11 @@ if make_map:
         # axes at bottom for colorbar
         divider = make_axes_locatable(ax)
         if map_dataset == 'Chapman':
-            bcax = divider.append_axes("bottom", size="7%", pad=0.1)
+            bcax = divider.append_axes("bottom", size="7%", pad=0.2)
         else:
             bcax = None
+        # right axis for rolling average of AF site coverage
+        rax = divider.append_axes('right', size='10%', pad=0)
         # plot land underneath
         countries_hi_res.to_crs(crs).plot(color='#f7f7f7',
                                           linewidth=0.25,
@@ -633,14 +634,16 @@ if make_map:
         if map_dataset == 'Chapman':
             pct_in_chap = 100*np.sum(af_locs['in_chap'])/len(af_locs)
             pct_in_chap_in_af = 100*np.mean(af_locs['chap_val'][af_locs['in_chap']]>=5)
-            print(('\n\n%0.2f%% OF ALL SITES FALL WITHIN CHAPMAN DATA, '
+            print(('\n\n%0.2f%% OF ALL SITES FALL WITHIN CHAPMAN DATA '
+                   '(%0.2f%% MISSED), '
                    'AND %0.2f%% OF THOSE FALL WITHIN CHAPMAN \'AGROFORESTRY\''
                    ' PIXELS (i.e., PIXELS >= 5 Mg C ha^-1)\n\n') %
-                  (pct_in_chap, pct_in_chap_in_af))
+                  (pct_in_chap, 100-pct_in_chap, pct_in_chap_in_af))
         elif map_dataset == 'Lesiv':
             pct_in_lesiv = 100*np.sum(af_locs['in_lesiv'])/len(af_locs)
-            print(('\n\n%0.2f%% OF ALL SITES FALL WITHIN LESIV DATA'
-                   '\n\n') % pct_in_lesiv)
+            print(('\n\n%0.2f%% OF ALL SITES FALL WITHIN LESIV DATA '
+                   '(%0.2f%% MISSED)'
+                   '\n\n') % (pct_in_lesiv, 100-pct_in_lesiv))
 
         # fix the colorbar and set ticks and labels
         if map_dataset == 'Chapman':
@@ -658,11 +661,48 @@ if make_map:
         label = {'Chapman': 'A.', 'Lesiv': 'B.'}[map_dataset]
         ax.text(1.13*map_minx, 0.88*map_maxy, label, size=28, weight='bold', clip_on=False)
 
+        # plot rolling average of AF site omission at the right
+        if map_dataset == 'Chapman':
+            in_data_col = 'in_chap'
+        else:
+            in_data_col = 'in_lesiv'
+        rolling_lat = af_locs.to_crs(crs).sort_values(by='lat',
+                                                ascending=False).geometry.y
+        rolling_coverage_chap = af_locs.to_crs(crs).sort_values(by='lat',
+                    ascending=False)['in_chap'].rolling(window=50,
+                                                          min_periods=1,
+                                                          center=False).mean()
+        rolling_coverage_lesiv = af_locs.to_crs(crs).sort_values(by='lat',
+                    ascending=False)['in_lesiv'].rolling(window=50,
+                                                          min_periods=1,
+                                                          center=False).mean()
+        chap_alpha = 1
+        lesiv_alpha = 0.33
+        chap_linestyle = '-'
+        lesiv_linestyle = ':'
+        if map_dataset == 'Lesiv':
+            chap_alpha, lesiv_alpha = lesiv_alpha, chap_alpha
+            chap_linestyle, lesiv_linestyle = lesiv_linestyle, chap_linestyle
+        rax.plot(rolling_coverage_chap, rolling_lat, color='black',
+                 linewidth=1, alpha=chap_alpha, linestyle=chap_linestyle)
+        rax.plot(rolling_coverage_lesiv, rolling_lat, color='black',
+                 linewidth=1, alpha=lesiv_alpha, linestyle=lesiv_linestyle)
+        rax.set_xlim([0, 1])
+        rax.set_ylim(ax.get_ylim())
+        rax.set_xlabel('coverage', fontdict={'fontsize':13})
+        rax.set_xticks([0,0.5,1])
+        rax.tick_params(labelsize=12)
+        rax.set_ylabel('')
+        rax.set_yticks(())
+        rax.xaxis.set_label_position('top')
+        #rax.xaxis.tick_top()
+
+
     fig_0.subplots_adjust(left=0.06,
                           bottom=0,
                           right=0.98,
                           top=1,
-                          hspace=0.15,
+                          hspace=0,
                          )
     fig_0.show()
 

fig4_spatial_temporal_chars/make_legend_for_MRV_radii.py
@@ -28,7 +28,7 @@ col_3dec = '#8306bf'
 # load and preprocess data
 ##########################
 # read in our points collected from the lit
-af_locs = pd.read_excel(('../mapping/AF_locations_from_papers/'
+af_locs = pd.read_excel(('../fig3_maps_fig5_curr_vs_poten/AF_locations_from_papers/'
                          'Agroforestry Data Dec 2021_MERGED_METANALYSES.xlsx'),
                         sheet_name='S2 sites')
 
@@ -157,7 +157,7 @@ for dim, dim_list in standard.items():
 ##################################
 
 # assess variance in divergence from 1:1 line as fn of geo coord precision
-def calc_coord_precision(coord):
+def estimate_coord_precision(coord):
     ct = 0
     # return 0 if no decimals
     if '.' not in str(coord):
@@ -177,8 +177,8 @@ def calc_coord_precision(coord):
                 return ct
 
 def calc_coord_precision_column(df):
-    lat_prec = [calc_coord_precision(lat) for lat in df.lat]
-    lon_prec = [calc_coord_precision(lon) for lon in df.lon]
+    lat_prec = [estimate_coord_precision(lat) for lat in df.lat]
+    lon_prec = [estimate_coord_precision(lon) for lon in df.lon]
     prec_col = (np.array(lat_prec) + np.array(lon_prec))/2
     # reclass the numeric coord_prec
     class_bin_lefts= [1, 2, 3]